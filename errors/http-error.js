f4130ec0990e2adacfb2986ff74a4f981df15dab
Vincent Brouwers <vincentbrouwers9@gmail.com>
2017-11-20 23:14:57 +0100

    Fix HttpError returning incorrect message

--- errors/http-error.js
+++ errors/http-error.js
@@ -5,15 +5,19 @@ class HttpError extends ExtendableError {
   /**
    * Creates a new HttpError Object
    *
-   * @param {Number} statusCode The HTTP Status Code.
    * @param {String|Object} [message] An optional message to show the user when the error occurs.
+   * @param {Number} statusCode The HTTP Status Code.
+   * @param [options] {Object} optional extra properties
    */
-  constructor(message, statusCode) {
-    super(
-      message || http.STATUS_CODES[statusCode],
-      http.STATUS_CODES[statusCode].toUpperCase().replace(/ /g, '_'),
-      { statusCode },
-    );
+  constructor(message, statusCode, options) {
+    if (!Number.isNaN(message)) {
+      options = statusCode;
+      statusCode = message;
+      message = http.STATUS_CODES[statusCode];
+    }
+    const code = http.STATUS_CODES[statusCode].toUpperCase().replace(/ /g, '_');
+
+    super(message, code, { statusCode, ...options });
   }
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.ExtendableError = require('./errors/extendable-error');
 module.exports.NotImplementedError = require('./errors/not-implemented-error');
 module.exports.ArgumentError = require('./errors/argument-error');
+module.exports.HttpError = require('./errors/http-error');
